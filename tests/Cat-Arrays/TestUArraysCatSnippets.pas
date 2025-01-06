bc5cf11d8f5101d10690ab6832acf4fca8a74eb0
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2025-01-06 17:18:10 +0000

    Add tests for new Arrays routine and method.
    Added tests for new TArrayUtils.Reverse<T> method and ReverseByteArray
    routine.
    
    Regenerated UArraysCatSnippets unit using CodeSnip. The generated unit
    contains up to date versions of all snippets in the Arrays category.
    The unit was used as generated with no changes.

--- tests/Cat-Arrays/TestUArraysCatSnippets.pas
+++ tests/Cat-Arrays/TestUArraysCatSnippets.pas
@@ -12,14 +12,19 @@ type
     fSA0: TArray<string>;
     fSA1: TArray<string>;
     fSA2: TArray<string>;
+    fSA2R: TArray<string>;
     fSAM: TArray<string>;
     fSAN: TArray<string>;
+    fSAR: TArray<string>;
     fIA0: TArray<Integer>;
     fIA1: TArray<Integer>;
     fIA2: TArray<Integer>;
+    fIA3: TArray<Integer>;
+    fIA3R: TArray<Integer>;
     fIAM: TArray<Integer>;
     fIAN: TArray<Integer>;
     fIAP: TArray<Integer>;
+    fIAR: TArray<Integer>;
     fIAX: TArray<Integer>;
     fIAY: TArray<Integer>;
   protected
@@ -30,12 +35,16 @@ type
     procedure TestLast;
     procedure TestIndexOf;
     procedure TestEqual;
+    // TestReverse must come after TestEqual since the test calls TArrayUtils.Equal<T>
+    procedure TestReverse;
     procedure TestSameStart;
   end;
 
   TestArraysCatSnippets = class(TTestCase)
   published
     procedure TestByteArraysEqual;
+    // The following test must come after TestByteArraysEqual since the test calls it
+    procedure TestReverseByteArray;
   end;
 
 implementation
@@ -54,13 +63,18 @@ begin
   fSA0 := TArray<string>.Create();
   fSA1 := TArray<string>.Create('foo');
   fSA2 := TArray<string>.Create('foo', 'bar');
+  fSA2R := TArray<string>.Create('bar', 'foo');
   fSAM := TArray<string>.Create('a', 'stitch', 'in', 'time', 'saves', 'nine');
   fSAN := TArray<string>.Create('a', 'stitch', 'in', 'time', 'saves', 'nine');
+  fSAR := TArray<string>.Create('nine', 'saves', 'time', 'in', 'stitch', 'a');
   fIA0 := TArray<Integer>.Create();
   fIA1 := TArray<Integer>.Create(42);
   fIA2 := TArray<Integer>.Create(42, 56);
+  fIA3 := TArray<Integer>.Create(56, 42, 102);
+  fIA3R := TArray<Integer>.Create(102, 42, 56);
   fIAM := TArray<Integer>.Create(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37);
   fIAN := TArray<Integer>.Create(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37);
+  fIAR := TArray<Integer>.Create(37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2, 1);
   fIAP := TArray<Integer>.Create(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
   fIAX := TArray<Integer>.Create(1, 2, 3, 5, 4, 11, 13, 17, 19, 23, 29, 31);
   fIAY := TArray<Integer>.Create(0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37);
@@ -153,6 +167,25 @@ begin
   CheckEquals(37, TArrayUtils.Last<Integer>(fIAN), 'Test 6');
 end;
 
+procedure TestTArrayUtils.TestReverse;
+var
+  RS: TArray<string>;
+  RI: TArray<Integer>;
+begin
+  RS := TArrayUtils.Reverse<string>(fSAM);
+  CheckTrue(TArrayUtils.Equal<string>(fSAR, RS, StringCompareFn), 'Test 1');
+  RI := TArrayUtils.Reverse<Integer>(fIAM);
+  CheckTrue(TArrayUtils.Equal<Integer>(fIAR, RI, IntegerCompareFn), 'Test 2');
+  RS := TArrayUtils.Reverse<string>(fSA2);
+  CheckTrue(TArrayUtils.Equal<string>(fSA2R, RS, StringCompareFn), 'Test 3');
+  RI := TArrayUtils.Reverse<Integer>(fIA0);
+  CheckTrue(TArrayUtils.Equal<Integer>(fIA0, RI, IntegerCompareFn), 'Test 4');
+  RI := TArrayUtils.Reverse<Integer>(fIA1);
+  CheckTrue(TArrayUtils.Equal<Integer>(fIA1, RI, IntegerCompareFn), 'Test 5');
+  RI := TArrayUtils.Reverse<Integer>(fIA3);
+  CheckTrue(TArrayUtils.Equal<Integer>(fIA3R, RI, IntegerCompareFn), 'Test 6');
+end;
+
 procedure TestTArrayUtils.TestSameStart;
 begin
   CheckTrue(
@@ -231,6 +264,32 @@ begin
   CheckFalse(ByteArraysEqual(A1L, ANRneq4), '#8');
   CheckFalse(ByteArraysEqual(A1L, AMR), '#9');
   CheckFalse(ByteArraysEqual(A0L, A1L), '#10');
+end;
+
+procedure TestArraysCatSnippets.TestReverseByteArray;
+var
+  A0, A1, A2, A6, A7, A4Sym, A5Sym: TBytes;
+  R0, R1, R2, R6, R7: TBytes;
+begin
+  SetLength(A0, 0);
+  SetLength(R0, 0);
+  A1 := TBytes.Create(42);
+  R1 := TBytes.Create(42);
+  A2 := TBytes.Create(42,56);
+  R2 := TBytes.Create(56,42);
+  A6 := TBytes.Create(1, 1, 2, 3, 5, 8);
+  R6 := TBytes.Create(8, 5, 3, 2, 1, 1);
+  A7 := TBytes.Create(0, 1, 1, 2, 3, 5, 8);
+  R7 := TBytes.Create(8, 5, 3, 2, 1, 1, 0);
+  A4Sym := TBytes.Create(3, 5, 5, 3);
+  A5Sym := TBytes.Create(3, 5, 8, 5, 3);
+  CheckTrue(ByteArraysEqual(R0, ReverseByteArray(A0)), '#0');
+  CheckTrue(ByteArraysEqual(R1, ReverseByteArray(A1)), '#1');
+  CheckTrue(ByteArraysEqual(R2, ReverseByteArray(A2)), '#2');
+  CheckTrue(ByteArraysEqual(R6, ReverseByteArray(A6)), '#6');
+  CheckTrue(ByteArraysEqual(R7, ReverseByteArray(A7)), '#7');
+  CheckTrue(ByteArraysEqual(A4Sym, ReverseByteArray(A4Sym)), '#4 sym');
+  CheckTrue(ByteArraysEqual(A5Sym, ReverseByteArray(A5Sym)), '#5 sym');
 end;
 
 initialization

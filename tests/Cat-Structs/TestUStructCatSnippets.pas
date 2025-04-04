77e27c206cb9cf349fc07248d75ea562105a27ef
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2025-04-04 12:58:05 +0100

    Add more unit tests for structs category snippets
    Added tests to TestUStructCatSnippets unit for all snippets in the
    Structures category except for TIntegerRange, which was the only snippet
    in the category to have tests.
    
    Advanced records had dedicated unit test classes. The remaining simple
    record types and functions had tests grouped together in a single new
    unit test class.

--- tests/Cat-Structs/TestUStructCatSnippets.pas
+++ tests/Cat-Structs/TestUStructCatSnippets.pas
@@ -7,7 +7,42 @@ uses
 
 implementation
 
+uses
+  Types;
+
 type
+
+  TestMiscStructsCatSnippets = class(TTestCase)
+  published
+    procedure TestType_TPointF;
+    procedure TestType_TRange;
+    procedure TestType_TRectF;      // requires TPointF
+    procedure TestFunction_PointF;  // requires TPointF
+    procedure TestFunction_Range;   // requires TRange
+    procedure TestFunction_RectF;   // requires TPointF & TRectF
+    procedure TestFunction_Size;
+    procedure TestFunction_BoundsF; // requires TRectF & RectF
+  end;
+
+  TestTSizeEx = class(TTestCase)
+  published
+    // Order of tests is important Ctor then implicit ops then equality ops and
+    // IsZero method
+    procedure TestCtorAndFields;
+    procedure TestImplicitOp_TSizeToTSizeEx;
+    procedure TestImplicitOp_TSizeExToTSize;
+    procedure TestEqualOp;
+    procedure TestNotEqualOp;
+    procedure TestIsZero;
+  end;
+
+  TestTRangeEx = class(TTestCase)
+  published
+    procedure TestCtorAndFields;
+    procedure TestContains;
+    procedure TestConstrain;
+  end;
+
   TestTIntegerRange = class(TTestCase)
   private
     procedure TestCtorAndPropsException;
@@ -34,6 +69,316 @@ type
     procedure TestIsContiguousWith;
     procedure TestIsContinuousWith;
   end;
+
+{ TestMiscStructsCatSnippets }
+
+procedure TestMiscStructsCatSnippets.TestFunction_BoundsF;
+var
+  R, Expected: TRectF;
+const
+  Delta = 0.00000001;
+begin
+  R := BoundsF(10.3, 20.4, 10.5, 20.6);
+  Expected := RectF(10.3, 20.4, 10.3+10.5, 20.4+20.6);
+  CheckEquals(Expected.Left, R.Left, Delta, 'left');
+  CheckEquals(Expected.Top, R.Top, Delta, 'top');
+  CheckEquals(Expected.Right, R.Right, Delta, 'right');
+  CheckEquals(Expected.Bottom, R.Bottom, Delta, 'bottom');
+end;
+
+procedure TestMiscStructsCatSnippets.TestFunction_PointF;
+var
+  P: TPointF;
+const
+  Delta = 0.00000001;
+begin
+  P := PointF(0.0, 0.0);
+  CheckEquals(0.0, P.X, Delta, '#1X');
+  CheckEquals(0.0, P.Y, Delta, '#1Y');
+  P := PointF(42.56, -12.345);
+  CheckEquals(42.56, P.X, Delta, '#2X');
+  CheckEquals(-12.345, P.Y, Delta, '#2Y');
+end;
+
+procedure TestMiscStructsCatSnippets.TestFunction_Range;
+var
+  R: TRange;
+begin
+  // Range function orders parameters
+  R := Range(42, 56);
+  CheckEquals(42, R.Lower, '#1 lower');
+  CheckEquals(56, R.Upper, '#1 upper');
+  R := Range(56, 42);
+  CheckEquals(42, R.Lower, '#2 lower');
+  CheckEquals(56, R.Upper, '#2 upper');
+end;
+
+procedure TestMiscStructsCatSnippets.TestFunction_RectF;
+var
+  R: TRectF;
+const
+  Delta = 0.00000001;
+begin
+  R := RectF(0.0, -10.8, 34.56, 20.3);
+  CheckEquals(0.0, R.Left, Delta, 'left');
+  CheckEquals(-10.8, R.Top, Delta, 'top');
+  CheckEquals(34.56, R.Right, Delta, 'right');
+  CheckEquals(20.3, R.Bottom, Delta, 'bottom');
+  CheckEquals(0.0, R.TopLeft.X, Delta, 'topleft.x');
+  CheckEquals(-10.8, R.TopLeft.Y, Delta, 'topleft.y');
+  CheckEquals(34.56, R.BottomRight.X, Delta, 'bottomright.x');
+  CheckEquals(20.3, R.BottomRight.Y, Delta, 'bottomright.y');
+end;
+
+procedure TestMiscStructsCatSnippets.TestFunction_Size;
+var
+  S: TSize;
+begin
+  S.cx := 42;
+  S.cy := 56;
+  CheckEquals(42, S.cx, 'cx');
+  CheckEquals(56, S.cy, 'cy');
+end;
+
+procedure TestMiscStructsCatSnippets.TestType_TPointF;
+var
+  P: TPointF;
+const
+  Delta = 0.00000001;
+begin
+  P.X := 0.0;
+  P.Y := 0.0;
+  CheckEquals(0.0, P.X, Delta, '#1X');
+  CheckEquals(0.0, P.Y, Delta, '#1Y');
+  P.X := 42.56;
+  P.Y := -12.345;
+  CheckEquals(42.56, P.X, Delta, '#2X');
+  CheckEquals(-12.345, P.Y, Delta, '#2Y');
+end;
+
+procedure TestMiscStructsCatSnippets.TestType_TRange;
+var
+  R: TRange;
+begin
+  // Test direct field setting
+  R.Lower := 42;
+  R.Upper := 56;
+  CheckEquals(42, R.Lower, '#1 lower');
+  CheckEquals(56, R.Upper, '#1 upper');
+  R.Lower := 56;
+  R.Upper := 42;
+  CheckEquals(56, R.Lower, '#2 lower');
+  CheckEquals(42, R.Upper, '#2 upper');
+end;
+
+procedure TestMiscStructsCatSnippets.TestType_TRectF;
+var
+  R: TRectF;
+  TL, BR: TPointF;
+const
+  Delta = 0.00000001;
+begin
+  // Set Left, Right, Top & Bottom fields
+  R.Left := 2.2;
+  R.Right := 6.6;
+  R.Top := 8.8;
+  R.Bottom := 16.16;
+  TL.X := 2.2;
+  TL.Y := 8.8;
+  BR.X := 6.6;
+  BR.Y := 16.16;
+  CheckEquals(2.2, R.Left, Delta, '#1 left');
+  CheckEquals(6.6, R.Right, Delta, '#1 right');
+  CheckEquals(8.8, R.Top, Delta, '#1 top');
+  CheckEquals(16.16, R.Bottom, Delta, '#1 bottom');
+  CheckEquals(2.2, R.TopLeft.X, Delta, '#1 topleft.x');
+  CheckEquals(8.8, R.TopLeft.Y, Delta, '#1 topleft.y');
+  CheckEquals(6.6, R.BottomRight.X, Delta, '#1 bottomright.x');
+  CheckEquals(16.16, R.BottomRight.Y, Delta, '#1 bottomright.y');
+  // Set TopLeft & BottomRight TPointF properties
+  TL.X := 10.11;
+  TL.Y := 12.13;
+  BR.X := 11.12;
+  BR.Y := 13.14;
+  R.TopLeft := TL;
+  R.BottomRight := BR;
+  CheckEquals(10.11, R.Left, Delta, '#2 left');
+  CheckEquals(12.13, R.Top, Delta, '#2 top');
+  CheckEquals(11.12, R.Right, Delta, '#2 right');
+  CheckEquals(13.14, R.Bottom, Delta, '#2 bottom');
+end;
+
+{ TestTSizeEx }
+
+procedure TestTSizeEx.TestCtorAndFields;
+var
+  S: TSizeEx;
+begin
+  // Test direct field access
+  S.CX := 42;
+  S.CY := -56;
+  CheckEquals(42, S.CX, '#1a');
+  CheckEquals(-56, S.CY, '#1b');
+  // Text Ctor
+  S := TSizeEx.Create(42, -56);
+  CheckEquals(42, S.CX, '#2a');
+  CheckEquals(-56, S.CY, '#2b');
+end;
+
+procedure TestTSizeEx.TestEqualOp;
+var
+  Sx0, Sx1a, Sx1b, Sx2: TSizeEx;
+  S0, S1, S2: TSize;
+begin
+  // Test with both operands TSizeEx
+  Sx0 := TSizeEx.Create(0, 0);
+  Sx1a := TSizeEx.Create(42, 56);
+  Sx1b := TSizeEx.Create(42, 56);
+  Sx2 := TSizeEx.Create(99, 99);
+  CheckTrue(Sx1a = Sx1b, '#1a');
+  CheckFalse(Sx0 = Sx2, '#1b');
+  CheckFalse(Sx1a = Sx2, '#1c');
+  // Test with one TSizeEx and one TSize operanc
+  S0 := Sx0;
+  S1 := Sx1a;
+  S2 := Sx2;
+  CheckTrue(Sx1a = S1, '#2a');
+  CheckFalse(S0 = Sx2, '#2b');
+  CheckTrue(S2 = Sx2, '#2c');
+end;
+
+procedure TestTSizeEx.TestImplicitOp_TSizeExToTSize;
+var
+  Src: TSizeEx;
+  Dest: TSize;
+begin
+  Src := TSizeEx.Create(23, -99);
+  Dest := Src;
+  CheckEquals(23, Dest.cx, 'cx');
+  CheckEquals(-99, Dest.cy, 'cy');
+end;
+
+procedure TestTSizeEx.TestImplicitOp_TSizeToTSizeEx;
+var
+  Src: TSize;
+  Dest: TSizeEx;
+begin
+  Src := TSizeEx.Create(23, 423);
+  Dest := Src;
+  CheckEquals(23, Dest.CX, 'CX');
+  CheckEquals(423, Dest.CY, 'CY');
+end;
+
+procedure TestTSizeEx.TestIsZero;
+var
+  S: TSizeEx;
+begin
+  S := TSizeEx.Create(12, 23);
+  CheckFalse(S.IsZero, '#1');
+  S := TSizeEx.Create(0, 0);
+  CheckTrue(S.IsZero, '#2');
+  S := TSizeEx.Create(0, 1);
+  CheckTrue(S.IsZero, '#3');
+  S := TSizeEx.Create(-1, 0);
+  CheckTrue(S.IsZero, '#4');
+end;
+
+procedure TestTSizeEx.TestNotEqualOp;
+var
+  Sx0, Sx1a, Sx1b, Sx2: TSizeEx;
+  S0, S1, S2: TSize;
+begin
+  // Test with both operands TSizeEx
+  Sx0 := TSizeEx.Create(0, 0);
+  Sx1a := TSizeEx.Create(42, 56);
+  Sx1b := TSizeEx.Create(42, 56);
+  Sx2 := TSizeEx.Create(99, 99);
+  CheckFalse(Sx1a <> Sx1b, '#1a');
+  CheckTrue(Sx0 <> Sx2, '#1b');
+  CheckTrue(Sx1a <> Sx2, '#1c');
+  // Test with one TSizeEx and one TSize operanc
+  S0 := Sx0;
+  S1 := Sx1a;
+  S2 := Sx2;
+  CheckFalse(Sx1a <> S1, '#2a');
+  CheckTrue(S0 <> Sx2, '#2b');
+  CheckFalse(S2 <> Sx2, '#2c');
+end;
+
+{ TestTRangeEx }
+
+procedure TestTRangeEx.TestConstrain;
+var
+  R: TRangeEx;
+begin
+  // Min < Max => expected results
+  R := TRangeEx.Create(-42, 56);
+  CheckEquals(2, R.Constrain(2), '#1a');
+  CheckEquals(-42, R.Constrain(-42), '#1b');
+  CheckEquals(56, R.Constrain(56), '#1c');
+  CheckEquals(-42, R.Constrain(-99), '#1d');
+  CheckEquals(56, R.Constrain(99), '#1e');
+  // Min > Max => bonkers results !!!
+  R := TRangeEx.Create(56, 42);
+  CheckEquals(56, R.Constrain(2), '#2a');   // !!! should be 42
+  CheckEquals(56, R.Constrain(42), '#2b');  // !!! should be 42
+  CheckEquals(42, R.Constrain(56), '#2c');  // !!! should be 56
+  CheckEquals(56, R.Constrain(48), '#2d');  // !!! should be 48
+  CheckEquals(56, R.Constrain(40), '#2e');  // !!! should be 42
+  CheckEquals(42, R.Constrain(99), '#2f');  // !!! should be 56
+  // Min = Max => expected results
+  R := TRangeEx.Create(3, 3);
+  CheckEquals(3, R.Constrain(2), '#1a');
+  CheckEquals(3, R.Constrain(3), '#1b');
+  CheckEquals(3, R.Constrain(4), '#1c');
+end;
+
+procedure TestTRangeEx.TestContains;
+var
+  R: TRangeEx;
+begin
+  // Min < Max => expected results
+  R := TRangeEx.Create(-42, 56);
+  CheckTrue(R.Contains(2), '#1a');
+  CheckTrue(R.Contains(-42), '#1b');
+  CheckTrue(R.Contains(56), '#1c');
+  CheckFalse(R.Contains(-99), '#1d');
+  CheckFalse(R.Contains(57), '#1e');
+  // Max > Min => bonkers results !!!
+  R := TRangeEx.Create(56, 42);
+  CheckFalse(R.Contains(48), '#2a'); // !!! Should be True
+  CheckFalse(R.Contains(2), '#2b');
+  CheckFalse(R.Contains(99), '#2c');
+  CheckFalse(R.Contains(42), '#2b'); // !!! Should be True
+  CheckFalse(R.Contains(56), '#2b'); // !!! Should be True
+  // Min = Max => expected results
+  R := TRangeEx.Create(3, 3);
+  CheckFalse(R.Contains(2), '#3a');
+  CheckFalse(R.Contains(4), '#3b');
+  CheckTrue(R.Contains(3), '#3c');
+end;
+
+procedure TestTRangeEx.TestCtorAndFields;
+var
+  R: TRangeEx;
+begin
+  // Direct field access: no ordering of range
+  R.Min := 42;
+  R.Max := 56;
+  CheckEquals(42, R.Min, '#1 min');
+  CheckEquals(56, R.Max, '#1 max');
+  R.Min := 56;
+  R.Max := 42;
+  CheckEquals(56, R.Min, '#2 min');
+  CheckEquals(42, R.Max, '#2 max');
+  // Ctor: also no ordering of range
+  R := TRangeEx.Create(42, 56);
+  CheckEquals(42, R.Min, '#3 min');
+  CheckEquals(56, R.Max, '#3 max');
+  R := TRangeEx.Create(56, 42);
+  CheckEquals(56, R.Min, '#3 min');
+  CheckEquals(42, R.Max, '#3 max');
+end;
 
 { TestTIntegerRange }
 
@@ -650,7 +995,11 @@ begin
 end;
 
 initialization
+
   // Register any test cases with the test runner
   RegisterTest(TestTIntegerRange.Suite);
+  RegisterTest(TestTRangeEx.Suite);
+  RegisterTest(TestTSizeEx.Suite);
+  RegisterTest(TestMiscStructsCatSnippets.Suite);
 
 end.
